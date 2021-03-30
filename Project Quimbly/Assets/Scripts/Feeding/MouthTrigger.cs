d1d43ddf9cbff86cab9d94825c2156000e03b95d
Aftermathrar <46353186+Aftermathrar@users.noreply.github.com>
2021-03-29 21:57:55 -0700

    Added eating cooldown
    small addition to allow for time between bites

--- Project Quimbly/Assets/Scripts/Feeding/MouthTrigger.cs
+++ Project Quimbly/Assets/Scripts/Feeding/MouthTrigger.cs
@@ -9,9 +9,17 @@ namespace ProjectQuimbly.Feeding
     public class MouthTrigger : MonoBehaviour, IDragDestination
     {
         [SerializeField] int biteSize = 1;
+        [SerializeField][Range(0, 3f)]
+        float eatingCooldown = 1f;
+        float timeSincelastBite = Mathf.Infinity;
         // Event set to cancel UI dragging in BasicFunctions
         // Tried canceling in the DragItem draghandler, but I couldn't get the PointerEventData to agree
         [SerializeField] UnityEvent onMouthEvent; 
+
+        private void Update() 
+        {
+            timeSincelastBite += Time.deltaTime;
+        }
         
         public float MaxAcceptable()
         {
@@ -20,11 +28,25 @@ namespace ProjectQuimbly.Feeding
 
         private void OnTriggerEnter2D(Collider2D other)
         {
-            SelectedFood food = other.GetComponent<SelectedFood>();
-            if(food != null)
+            TryToEat(other);
+        }
+
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            TryToEat(other);
+        }
+
+        private void TryToEat(Collider2D other)
+        {
+            if (timeSincelastBite >= eatingCooldown)
             {
-                food.RemoveItems(biteSize);
-                onMouthEvent?.Invoke();
+                SelectedFood food = other.GetComponent<SelectedFood>();
+                if (food != null)
+                {
+                    timeSincelastBite = 0;
+                    food.RemoveItems(biteSize);
+                    onMouthEvent?.Invoke();
+                }
             }
         }
     }
