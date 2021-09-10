d0786aba6e5e41c3cc77e175a2e21af9bc5d4cd4
GamingTVreal <mark.h10156@gmail.com>
2021-09-09 22:21:03 -0700

    UI Overhaul + More settings options (1)

--- Project Quimbly/Assets/SettingsMenu.cs
+++ Project Quimbly/Assets/SettingsMenu.cs
@@ -26,4 +26,16 @@ public class SettingsMenu : MonoBehaviour
             Screen.fullScreen = false;
         }
     }
+    List<int> widths = new List<int>() { 568, 960, 1280, 1920 };
+    List<int> heights = new List<int>() { 329, 540, 800, 1080 };
+
+    public void SetScreenSize(int index)
+    {
+        bool fullscreen = Screen.fullScreen;
+        int width = widths[index];
+        int height = heights[index];
+        Screen.SetResolution(width, height, fullscreen);
+
+    }
+
 }
