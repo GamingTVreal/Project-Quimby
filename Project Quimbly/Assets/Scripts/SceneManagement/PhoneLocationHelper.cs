5e4c2ececff82c0fad918540af139fea31cb17c1
Aftermathrar <46353186+Aftermathrar@users.noreply.github.com>
2021-09-13 20:00:56 -0700

    Update PhoneLocationHelper.cs

--- Project Quimbly/Assets/Scripts/SceneManagement/PhoneLocationHelper.cs
+++ Project Quimbly/Assets/Scripts/SceneManagement/PhoneLocationHelper.cs
@@ -6,6 +6,16 @@ namespace ProjectQuimbly.SceneManagement
 {
     public class PhoneLocationHelper : MonoBehaviour
     {
+        public void LoadLocation(string newLocation)
+        {
+            GameObject controllerGO = GameObject.FindWithTag("GameController");
+            if (controllerGO != null)
+            {
+                LoadingScreenScript loadingScript = controllerGO.GetComponent<LoadingScreenScript>();
+                loadingScript.LoadNewArea(newLocation);
+            }
+        }
+
         public void ReturnToMainMenu()
         {
             GameObject controllerGO = GameObject.FindWithTag("GameController");
