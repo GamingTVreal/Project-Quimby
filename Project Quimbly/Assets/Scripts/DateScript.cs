776f483ec9c9b25cce857b7ac55106fea7d6db06
GamingTVreal <mark.h10156@gmail.com>
2021-09-24 08:54:34 -0700

    minor bugfixes

--- Project Quimbly/Assets/Scripts/DateScript.cs
+++ Project Quimbly/Assets/Scripts/DateScript.cs
@@ -27,7 +27,7 @@ public class DateScript : MonoBehaviour
     {
         Fill.color = Gradient.Evaluate(dateSlider.normalizedValue);
         dateLevel = girlController.GetDateLevel();
-        dateLevel = 2; //Comment this out 
+       // dateLevel = 2; //Comment this out 
         conversant.StartDialogue("Date " + dateLevel);
     }
 
